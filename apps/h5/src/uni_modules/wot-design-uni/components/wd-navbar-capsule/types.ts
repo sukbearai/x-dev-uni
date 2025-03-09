c69e242d7633b86d9a9df7dd9b4f43792d3660ca
suk.bear <suk.bear.suwu@gmail.com>
2025-03-09 09:27:23 +0800

    chore: remove h5

--- apps/h5/src/uni_modules/wot-design-uni/components/wd-navbar-capsule/types.ts
+++
@@ -1,8 +0,0 @@
-import type { ExtractPropTypes } from 'vue'
-import { baseProps } from '../common/props'
-
-export const navbarCapsuleProps = {
-  ...baseProps
-}
-
-export type NavbarCapsuleProps = ExtractPropTypes<typeof navbarCapsuleProps>
